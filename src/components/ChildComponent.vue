1ccf3f79819f21399565583ce708e4b282d5eca2
Rownak-Abin <rownak@annonlab.com>
2023-08-20 20:31:14 +0600

    first commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,34 @@
+
+
+<template>
+  <div>
+    <ParentComponent :message="message" @messageEmitted="handleMessageEmitted" />
+    <p>Emitted Message from ParentComponent: {{ emittedMessage }}</p>
+    <input type="text" v-model="emittedMessage">
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import ParentComponent from './ParentComponent.vue';
+
+export default defineComponent({
+  components: {
+    ParentComponent
+  },
+  setup() {
+    const message = ref("Hello from ChildComponent");
+    const emittedMessage = ref("");
+
+    const handleMessageEmitted = (message) => {
+      emittedMessage.value = message;
+    };
+
+    return {
+      message,
+      emittedMessage,
+      handleMessageEmitted
+    };
+  }
+});
+</script>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Received Message from Child Component: {{ message }}</p>
+    <button @click="emitMessage">Emit Message</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent, defineEmits, withDefaults } from 'vue';
+
+const emits = withDefaults(defineEmits(), ['messageEmitted']);
+
+export default defineComponent({
+  props: {
+    message: String
+  },
+  setup(props) {
+    const emitMessage = () => {
+      emits.messageEmitted('Custom message emitted from Parent Component');
+    };
+
+    return {
+      emitMessage
+    };
+  }
+});
+</script>
